a434dd0b0ae64b948e2a24e435f8c33b17802d5d
l24co069n <l24co069@rvrjc.ac.in>
2025-06-04 22:30:09 +0530

    Updated index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -59,7 +59,7 @@ const AppRoutes = () => {
           <Route index element={<CourseList />} />
           {user?.role === 'instructor' && (
             <>
-              <Route path="create" element={<CreateCourse />} />
+              <Route path="create\" element={<CreateCourse />} />
               <Route path=":id/edit" element={<EditCourse />} />
             </>
           )}
@@ -70,7 +70,7 @@ const AppRoutes = () => {
           <Route index element={<ChapterList />} />
           {user?.role === 'instructor' && (
             <>
-              <Route path="create" element={<CreateChapter />} />
+              <Route path="create\" element={<CreateChapter />} />
               <Route path=":chapterId/edit" element={<EditChapter />} />
             </>
           )}
@@ -79,7 +79,7 @@ const AppRoutes = () => {
         <Route path="lessons/:courseId/:chapterId">
           {user?.role === 'instructor' && (
             <>
-              <Route path="create" element={<CreateLesson />} />
+              <Route path="create\" element={<CreateLesson />} />
               <Route path=":lessonId/edit" element={<EditLesson />} />
             </>
           )}
@@ -90,7 +90,7 @@ const AppRoutes = () => {
           <Route index element={<QuizList />} />
           {user?.role === 'instructor' && (
             <>
-              <Route path="create" element={<CreateQuiz />} />
+              <Route path="create\" element={<CreateQuiz />} />
               <Route path=":id/edit" element={<EditQuiz />} />
             </>
           )}
@@ -102,7 +102,7 @@ const AppRoutes = () => {
           <Route index element={<AssessmentList />} />
           {user?.role === 'instructor' && (
             <>
-              <Route path="create" element={<CreateAssessment />} />
+              <Route path="create\" element={<CreateAssessment />} />
               <Route path=":id/submissions" element={<AssessmentSubmissions />} />
             </>
           )}
